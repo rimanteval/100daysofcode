86bde40e23dd8473958ff0d8b4e4287a0773e6d1
rimanteval <rvalancauskaite@gmail.com>
2019-08-19 18:52:34 +0300

    day 1 final

--- day1/app/src/main/java/rimante/day1/Main2Activity.java
+++ day1/app/src/main/java/rimante/day1/Main2Activity.java
@@ -2,13 +2,33 @@ package rimante.day1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+    private Button btn2;
+    private TextView text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        text2 = (TextView) findViewById(R.id.tv2);
+        int num = 1 + (int) (Math.random() * ((1000 - 1) + 1));
+        text2.setText(num + "");
+
+        btn2 = (Button) findViewById(R.id.b2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(Main2Activity.this, MainActivity.class);
+                Main2Activity.this.startActivity(intent1);
+            }
+        });
+
     }
 }
--- day1/app/src/main/java/rimante/day1/MainActivity.java
+++ day1/app/src/main/java/rimante/day1/MainActivity.java
@@ -8,48 +8,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
     private Button btn1;
-    private Button btn2;
     private TextView text1;
-    private TextView text2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         text1 = (TextView) findViewById(R.id.tv1);
-        text1.setText(1 + (int)(Math.random() * ((1000 - 1) + 1)));
-
-        text2 = (TextView) findViewById(R.id.tv2);
-        text2.setText(1 + (int)(Math.random() * ((1000 - 1) + 1)));
+        int num = 1 + (int) (Math.random() * ((1000 - 1) + 1));
+        text1.setText(num + "");
 
         btn1 = (Button) findViewById(R.id.b1);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivityB();
+                Intent intent2 = new Intent(MainActivity.this, Main2Activity.class);
+                MainActivity.this.startActivity(intent2);
             }
         });
 
-        btn2 = (Button) findViewById(R.id.b2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivityA();
-            }
-        });
     }
 
-    public void openActivityA(){
-        Intent intent1 = new Intent(this, MainActivity.class);
-        startActivity(intent1);
-    }
-
-    public void openActivityB(){
-        Intent intent2 = new Intent(this,Main2Activity.class);
-        startActivity(intent2);
-    }
 }
